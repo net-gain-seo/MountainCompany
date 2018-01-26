c53d9ce659b24ea63bb8f1813230b9ed31a2adcf
Mitchell Tracy <mitchell@netgainseo.com>
2018-01-26 16:47:05 -0500

    Home Page Complete

--- archive-testimonials.php
+++ archive-testimonials.php
@@ -16,15 +16,12 @@
 
         <div class="container">
             <article>
-
-                <div class="the-testimonial">
-                    <?php the_content(); ?>
-                </div>
-
                 <div class="txt-primary">
                     <?php the_title(); ?>
                 </div>
-
+                <div class="the-testimonial">
+                    <?php the_content(); ?>
+                </div>
 
             </article>
         </div>
