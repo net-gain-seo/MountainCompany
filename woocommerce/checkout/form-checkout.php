6331a18ffbc27e2739d454619f64226b4cefbe63
Mitchell Tracy <mitchell@netgainseo.com>
2018-02-07 16:59:15 -0500

    Woocommerce Final

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -18,22 +18,21 @@
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
-}
-
-wc_print_notices();
-
-do_action( 'woocommerce_before_checkout_form', $checkout );
-
-// If checkout registration is disabled and not logged in, the user cannot checkout
-if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_required() && ! is_user_logged_in() ) {
-	echo apply_filters( 'woocommerce_checkout_must_be_logged_in_message', __( 'You must be logged in to checkout.', 'woocommerce' ) );
-	return;
-}
-
-?>
+}?>
 <div class="container">
 	<div class="row">
-		<div class="col col-lg-12 col-md-12 col-sm-12 col-xsm-12 ">
+		<?php
+		wc_print_notices();
+		do_action( 'woocommerce_before_checkout_form', $checkout );
+		// If checkout registration is disabled and not logged in, the user cannot checkout
+		if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_required() && ! is_user_logged_in() ) {
+			echo apply_filters( 'woocommerce_checkout_must_be_logged_in_message', __( 'You must be logged in to checkout.', 'woocommerce' ) );
+			return;
+		}
+		?>
+	
+
+	<div class="col col-lg-12 col-md-12 col-sm-12 col-xsm-12 ">
 <form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
 
 	<?php if ( $checkout->get_checkout_fields() ) : ?>
@@ -68,7 +67,6 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 
 <?php do_action( 'woocommerce_after_checkout_form', $checkout ); ?>
 
-
-</div>
-</div>
+		</div>
+	</div>
 </div>
